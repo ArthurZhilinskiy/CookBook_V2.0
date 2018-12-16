f4e3968f359156e617267aad1ad04061819c810d
Arthur Zhilinskiy <zjilka22@mail.ru>
2018-12-17 00:22:32 +0300

    Главная форма готова и структура БД

--- CookBook_V2.0/CookBook_V2.0/Carts.cs
+++ CookBook_V2.0/CookBook_V2.0/Carts.cs
@@ -1,22 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data.SqlClient;
+using System.IO;
+using System.Collections;
+using System.Diagnostics;
+using System.Management;
+
 namespace CookBook_V2._0
 {
-    public partial class Form1 : MetroFramework.Forms.MetroForm
+    public partial class Carts : UserControl
     {
-        public Form1()
+        public Carts()
         {
             InitializeComponent();
         }
-
-      
     }
 }
--- CookBook_V2.0/CookBook_V2.0/Form1.Designer.cs
+++ CookBook_V2.0/CookBook_V2.0/Form1.Designer.cs
@@ -28,20 +28,298 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.metroPanel2 = new MetroFramework.Controls.MetroPanel();
+            this.ml1 = new MetroFramework.Controls.MetroLink();
+            this.ml2 = new MetroFramework.Controls.MetroLink();
+            this.ml10 = new MetroFramework.Controls.MetroLink();
+            this.ml9 = new MetroFramework.Controls.MetroLink();
+            this.ml8 = new MetroFramework.Controls.MetroLink();
+            this.ml7 = new MetroFramework.Controls.MetroLink();
+            this.ml6 = new MetroFramework.Controls.MetroLink();
+            this.ml5 = new MetroFramework.Controls.MetroLink();
+            this.ml4 = new MetroFramework.Controls.MetroLink();
+            this.ml3 = new MetroFramework.Controls.MetroLink();
+            this.mtbSearch = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.flp1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.metroPanel1.SuspendLayout();
+            this.metroPanel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.metroPanel1.Controls.Add(this.metroTile1);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(23, 63);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(50, 372);
+            this.metroPanel1.TabIndex = 0;
+            this.metroPanel1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // metroPanel2
+            // 
+            this.metroPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroPanel2.Controls.Add(this.ml1);
+            this.metroPanel2.Controls.Add(this.ml2);
+            this.metroPanel2.Controls.Add(this.ml10);
+            this.metroPanel2.Controls.Add(this.ml9);
+            this.metroPanel2.Controls.Add(this.ml8);
+            this.metroPanel2.Controls.Add(this.ml7);
+            this.metroPanel2.Controls.Add(this.ml6);
+            this.metroPanel2.Controls.Add(this.ml5);
+            this.metroPanel2.Controls.Add(this.ml4);
+            this.metroPanel2.Controls.Add(this.ml3);
+            this.metroPanel2.Controls.Add(this.mtbSearch);
+            this.metroPanel2.Controls.Add(this.metroLabel1);
+            this.metroPanel2.Controls.Add(this.flp1);
+            this.metroPanel2.HorizontalScrollbarBarColor = true;
+            this.metroPanel2.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel2.HorizontalScrollbarSize = 10;
+            this.metroPanel2.Location = new System.Drawing.Point(79, 63);
+            this.metroPanel2.Name = "metroPanel2";
+            this.metroPanel2.Size = new System.Drawing.Size(560, 372);
+            this.metroPanel2.TabIndex = 0;
+            this.metroPanel2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroPanel2.VerticalScrollbarBarColor = true;
+            this.metroPanel2.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel2.VerticalScrollbarSize = 10;
+            // 
+            // ml1
+            // 
+            this.ml1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ml1.Location = new System.Drawing.Point(409, 76);
+            this.ml1.Name = "ml1";
+            this.ml1.Size = new System.Drawing.Size(137, 23);
+            this.ml1.TabIndex = 5;
+            this.ml1.Text = "Первые блюда";
+            this.ml1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ml1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.ml1.UseSelectable = true;
+            // 
+            // ml2
+            // 
+            this.ml2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ml2.Location = new System.Drawing.Point(409, 105);
+            this.ml2.Name = "ml2";
+            this.ml2.Size = new System.Drawing.Size(137, 23);
+            this.ml2.TabIndex = 5;
+            this.ml2.Text = "Вторые блюда";
+            this.ml2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ml2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.ml2.UseSelectable = true;
+            // 
+            // ml10
+            // 
+            this.ml10.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ml10.Location = new System.Drawing.Point(409, 337);
+            this.ml10.Name = "ml10";
+            this.ml10.Size = new System.Drawing.Size(137, 23);
+            this.ml10.TabIndex = 5;
+            this.ml10.Text = "Блины и оладьи";
+            this.ml10.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ml10.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.ml10.UseSelectable = true;
+            // 
+            // ml9
+            // 
+            this.ml9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ml9.Location = new System.Drawing.Point(409, 308);
+            this.ml9.Name = "ml9";
+            this.ml9.Size = new System.Drawing.Size(137, 23);
+            this.ml9.TabIndex = 5;
+            this.ml9.Text = "Торты";
+            this.ml9.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ml9.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.ml9.UseSelectable = true;
+            // 
+            // ml8
+            // 
+            this.ml8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ml8.Location = new System.Drawing.Point(409, 279);
+            this.ml8.Name = "ml8";
+            this.ml8.Size = new System.Drawing.Size(137, 23);
+            this.ml8.TabIndex = 5;
+            this.ml8.Text = "Выпечка";
+            this.ml8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ml8.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.ml8.UseSelectable = true;
+            // 
+            // ml7
+            // 
+            this.ml7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ml7.Location = new System.Drawing.Point(409, 250);
+            this.ml7.Name = "ml7";
+            this.ml7.Size = new System.Drawing.Size(137, 23);
+            this.ml7.TabIndex = 5;
+            this.ml7.Text = "Десерты";
+            this.ml7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ml7.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.ml7.UseSelectable = true;
+            // 
+            // ml6
+            // 
+            this.ml6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ml6.Location = new System.Drawing.Point(409, 221);
+            this.ml6.Name = "ml6";
+            this.ml6.Size = new System.Drawing.Size(137, 23);
+            this.ml6.TabIndex = 5;
+            this.ml6.Text = "Напитки";
+            this.ml6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ml6.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.ml6.UseSelectable = true;
+            // 
+            // ml5
+            // 
+            this.ml5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ml5.Location = new System.Drawing.Point(409, 192);
+            this.ml5.Name = "ml5";
+            this.ml5.Size = new System.Drawing.Size(137, 23);
+            this.ml5.TabIndex = 5;
+            this.ml5.Text = "Соусы, кремы";
+            this.ml5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ml5.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.ml5.UseSelectable = true;
+            // 
+            // ml4
+            // 
+            this.ml4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ml4.Location = new System.Drawing.Point(409, 163);
+            this.ml4.Name = "ml4";
+            this.ml4.Size = new System.Drawing.Size(137, 23);
+            this.ml4.TabIndex = 5;
+            this.ml4.Text = "Салаты";
+            this.ml4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ml4.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.ml4.UseSelectable = true;
+            // 
+            // ml3
+            // 
+            this.ml3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ml3.Location = new System.Drawing.Point(409, 134);
+            this.ml3.Name = "ml3";
+            this.ml3.Size = new System.Drawing.Size(137, 23);
+            this.ml3.TabIndex = 5;
+            this.ml3.Text = "Закуски";
+            this.ml3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ml3.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.ml3.UseSelectable = true;
+            // 
+            // mtbSearch
+            // 
+            this.mtbSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            // 
+            // 
+            // 
+            this.mtbSearch.CustomButton.Image = null;
+            this.mtbSearch.CustomButton.Location = new System.Drawing.Point(126, 1);
+            this.mtbSearch.CustomButton.Name = "";
+            this.mtbSearch.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.mtbSearch.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.mtbSearch.CustomButton.TabIndex = 1;
+            this.mtbSearch.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.mtbSearch.CustomButton.UseSelectable = true;
+            this.mtbSearch.CustomButton.Visible = false;
+            this.mtbSearch.Lines = new string[0];
+            this.mtbSearch.Location = new System.Drawing.Point(409, 9);
+            this.mtbSearch.MaxLength = 32767;
+            this.mtbSearch.Name = "mtbSearch";
+            this.mtbSearch.PasswordChar = '\0';
+            this.mtbSearch.PromptText = "Поиск рецепта";
+            this.mtbSearch.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.mtbSearch.SelectedText = "";
+            this.mtbSearch.SelectionLength = 0;
+            this.mtbSearch.SelectionStart = 0;
+            this.mtbSearch.ShortcutsEnabled = true;
+            this.mtbSearch.Size = new System.Drawing.Size(148, 23);
+            this.mtbSearch.TabIndex = 4;
+            this.mtbSearch.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.mtbSearch.UseSelectable = true;
+            this.mtbSearch.WaterMark = "Поиск рецепта";
+            this.mtbSearch.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.mtbSearch.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(409, 54);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(137, 19);
+            this.metroLabel1.TabIndex = 3;
+            this.metroLabel1.Text = "Категории рецептов:";
+            this.metroLabel1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // flp1
+            // 
+            this.flp1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flp1.Location = new System.Drawing.Point(3, 3);
+            this.flp1.Name = "flp1";
+            this.flp1.Size = new System.Drawing.Size(400, 366);
+            this.flp1.TabIndex = 2;
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.Location = new System.Drawing.Point(3, 9);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(44, 44);
+            this.metroTile1.Style = MetroFramework.MetroColorStyle.Teal;
+            this.metroTile1.TabIndex = 2;
+            this.metroTile1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTile1.TileImage = global::CookBook_V2._0.Properties.Resources.Add;
+            this.metroTile1.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile1.UseSelectable = true;
+            this.metroTile1.UseTileImage = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(750, 400);
+            this.ClientSize = new System.Drawing.Size(662, 458);
+            this.Controls.Add(this.metroPanel2);
+            this.Controls.Add(this.metroPanel1);
+            this.MinimumSize = new System.Drawing.Size(662, 458);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Кулинарная книга";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroPanel1.ResumeLayout(false);
+            this.metroPanel2.ResumeLayout(false);
+            this.metroPanel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private MetroFramework.Controls.MetroPanel metroPanel2;
+        private MetroFramework.Controls.MetroLink ml1;
+        private MetroFramework.Controls.MetroLink ml2;
+        private MetroFramework.Controls.MetroLink ml10;
+        private MetroFramework.Controls.MetroLink ml9;
+        private MetroFramework.Controls.MetroLink ml8;
+        private MetroFramework.Controls.MetroLink ml7;
+        private MetroFramework.Controls.MetroLink ml6;
+        private MetroFramework.Controls.MetroLink ml5;
+        private MetroFramework.Controls.MetroLink ml4;
+        private MetroFramework.Controls.MetroLink ml3;
+        private MetroFramework.Controls.MetroTextBox mtbSearch;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private System.Windows.Forms.FlowLayoutPanel flp1;
+        private MetroFramework.Controls.MetroTile metroTile1;
     }
 }
 
